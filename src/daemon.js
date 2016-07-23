c661150a36428590060220d6297024e10ea52951
Bailey Stoner <monokrome@monokro.me>
2016-07-22 18:29:16 -0700

    Convert sources to ES6.

---
+++ src/daemon.js
@@ -0,0 +1,46 @@
+const events = require('events'),
+      lodash = require('lodash'),
+      shared = require('inform-shared'),
+      zmq = require('zmq');
+
+
+class InvalidPluginError extends Error {}
+
+class Daemon extends events.EventEmitter {
+  listen() {
+    const socket = zmq.socket('pull');
+
+    socket.bindSync('tcp://127.0.0.1:5000');
+    socket.on('message', this.handle.bind(this));
+  }
+
+  handle(message) {
+    const notification = new shared.Notification;
+    notification.message = message;
+    this.emit('message', this.normalize(notification));
+  }
+
+  normalize(notification) {
+    return notification.get();
+  }
+
+  use(moduleName, options) {
+    const pluginModule = require(moduleName),
+          plugin = new pluginModule.Plugin(options);
+
+    if (typeof plugin.receive === 'undefined')
+      throw new InvalidPluginError(
+        "Expected plugin to (" + module + ") define a receive method."
+      );
+
+    this.on('message', plugin.receive.bind(plugin));
+
+    return plugin;
+  }
+}
+
+
+module.exports = {
+  InvalidPluginError,
+  Daemon,
+};
--- src/index.coffee
+++
@@ -1,5 +0,0 @@
-{Daemon} = require './daemon'
-
-module.exports = {
-  Daemon
-}
